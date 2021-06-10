8e929b2d97e20fccd8fcde7aa4d54d48928cbd5c
Matheus Guedes <matheushg67@hotmail.com>
2021-06-10 19:35:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -12,21 +12,16 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-    print('------')
+    print('Logged in as {0.user}'.format(client))
+
 
 async def my_background_task():
     await client.wait_until_ready()
-    counter = 0
     channel = client.get_channel(811733009044733962)
-    print(channel)
-    print('antes do while')
     while not client.is_closed():
-        print('got in while')
         ada_min = 1.60
         ada_max = 2
         response = req.get('https://api.binance.com/api/v3/ticker/price?symbol=ADAUSDT')
-        print("Value:", response.json())
         jsonRes = response.json()
         value_response = jsonRes['price']
         value = float(value_response)
@@ -41,4 +36,4 @@ async def my_background_task():
         await asyncio.sleep(30) # task runs every 30 seconds
 
 client.loop.create_task(my_background_task())
-client.run('Nzc4Nzc1NTczNzM5ODY0MTE0.X7W5RQ.yCGRpVj7IAuTzuCOZnakuSbwRDY')
+client.run(os.getenv('TOKEN'))
