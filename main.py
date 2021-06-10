848be412053c3cc8c6c00bf2cbd9097f9247d365
Matheus Guedes <matheushg67@hotmail.com>
2021-06-10 20:04:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ async def on_message(message):
     return
 
   if message.content.startswith('!preço'):
-    price = get_price()
+    price = await.get_price()
     await message.channel.send('Preço atual da ADA: %s' %price)
         
 client.loop.create_task(my_background_task())
